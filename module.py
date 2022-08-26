48a229da6bd233173a7fbbadeae96e5984cdeca6
Cyrus Yi <111038411+GenericCoderGuy@users.noreply.github.com>
2022-08-25 23:11:37 -0700

    First Commit

---
+++ module.py
@@ -0,0 +1,23 @@
+# def print_name(name):
+#     print(f"Hello {name}, we've been expecting you")
+
+# def adder(num1, num2):
+#     return num1 + num2
+
+# def calculate_circle(num1, num2):
+# r = float(input(' Please Enter the radius of a circle: '))
+# r = float(input(' Please Enter the radius of a circle: '))
+# area = 3.14 * r * r
+# circumference = 2 * PI * r
+# print(" Area Of a Circle = %.2f" %area)
+# print(" Circumference Of a Circle = %.2f" %circumference)
+
+
+def calculate_circle(num3):
+    print(float(2) * 3.14 * float(num3))
+
+def calculate_square(num1, num2):
+    print(num1 * num2)
+
+
+
